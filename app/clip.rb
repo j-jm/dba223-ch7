5661c49edfbac4baf26f11ebab245edaed0c5d53
Jean-Jacques Maury <jean0jacques@yahoo.com>
2015-10-15 10:27:56 -0400

    delete clip

--- app/clip.rb
+++
@@ -1,7 +0,0 @@
-  validates_format_of :faren, :with => /[0-9]/
-    validates_presence_of :faren, :message => 'Please, enter a temperature in F degrees'
-  validates_format_of :faren, :in => 0..9
-
-    def convert
-    @cel = (@faren - 32) * (5/9)
-  end
